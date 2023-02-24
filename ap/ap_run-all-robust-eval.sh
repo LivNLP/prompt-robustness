0a7a3c724f577b02e8736f1dc9aa2ab6b321fe07
yoichi1484 <ishibashi.yoichi.ir3@naist.jp>
2023-02-24 19:09:09 +0900

    Uncomment out AP script

--- ap/ap_run-all-robust-eval.sh
+++ ap/ap_run-all-robust-eval.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 task="cb"
-#python exp_original-accuracy.py --task "$task"
-#python exp_reordering.py --task "$task"
-#python exp_deletion.py --task "$task"
-#python exp_cross-dataset.py --task "$task"
-#python exp_adv-perturbation.py --task "$task"
+python exp_original-accuracy.py --task "$task"
+python exp_reordering.py --task "$task"
+python exp_deletion.py --task "$task"
+python exp_cross-dataset.py --task "$task"
+python exp_adv-perturbation.py --task "$task"
 
 
 task="mnli"
 python exp_original-accuracy.py --task "$task"
-#python exp_reordering.py --task "$task"
-#python exp_deletion.py --task "$task"
-#python exp_cross-dataset.py --task "$task"
-#python exp_adv-perturbation.py --task "$task"
+python exp_reordering.py --task "$task"
+python exp_deletion.py --task "$task"
+python exp_cross-dataset.py --task "$task"
+python exp_adv-perturbation.py --task "$task"
 
 
